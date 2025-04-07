5e40a3aefe7661ec1fc3d7e4d616fcdb59a7991c
ask-t <asktakahashi@go.byuh.edu>
2025-04-06 16:55:07 -1000

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: process.env.NODE_ENV === 'production' ? '/music465' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/music465' : '',
+};
 
 export default nextConfig;
